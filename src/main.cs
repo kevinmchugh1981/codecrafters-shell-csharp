892d1079fbe867f7c44fa6b2c2f57e36a9811eeb
Kevin <kevin.mchugh81@googlemail.com>
2026-01-21 14:07:44 +0000

    Second Task

--- src/main.cs
+++ src/main.cs
@@ -3,5 +3,7 @@ class Program
     static void Main()
     {
          Console.Write("$ ");
+         var command = Console.ReadLine();
+         Console.Out.WriteLine($"{command}: command not found");
     }
 }
