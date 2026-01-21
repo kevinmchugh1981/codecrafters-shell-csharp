afc8aaafbd3c36b74155473d3fbf5658cbc4c325
Kevin <kevin.mchugh81@googlemail.com>
2026-01-21 15:21:16 +0000

    Seventh Task

--- src/Commands/ExternalCommand.cs
+++ src/Commands/ExternalCommand.cs
@@ -7,7 +7,7 @@ public class ExternalCommand(string filePath) : ICommand
         var startInfo = new ProcessStartInfo(filePath);
         if (args.Length > 1)
         {
-            startInfo.Arguments = string.Join(" ", args);
+            startInfo.Arguments = string.Join(" ", args.Skip(1).ToArray());
         }
         Process.Start(startInfo);
     }
