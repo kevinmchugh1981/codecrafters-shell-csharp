a8ae471340196b219ef29fd17a5665e92ab6ebff
Kevin <kevin.mchugh81@googlemail.com>
2026-01-21 15:39:37 +0000

    Seventh Task

--- src/Commands/ExternalCommand.cs
+++ src/Commands/ExternalCommand.cs
@@ -7,10 +7,11 @@ public class ExternalCommand() : ICommand
         var command = args[0];
         var commandArgs = args.Length > 1 ? string.Join(" ", args.Skip(1).ToArray()) : string.Empty;
         
-        var startInfo = new ProcessStartInfo("/bin/sh", $"{command} {commandArgs}");
-        startInfo.RedirectStandardError = true;
-        startInfo.RedirectStandardError = true;
-        startInfo.UseShellExecute = false;
+        var startInfo = new ProcessStartInfo("/bin/sh", $"{command} {commandArgs}")
+        {
+            RedirectStandardError = true,
+            UseShellExecute = false
+        };
         using var process = Process.Start(startInfo);
         var output = process?.StandardOutput.ReadToEnd();
         var error = process?.StandardError.ReadToEnd();
