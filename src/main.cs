429cee987b00af153dceed178dfaf852e38db134
Kevin <kevin.mchugh81@googlemail.com>
2026-01-21 14:22:36 +0000

    Fourth Task

--- src/main.cs
+++ src/main.cs
@@ -2,17 +2,32 @@ class Program
 {
     static void Main()
     {
-        
-        for (string? command; (command= GetInput()) != "exit";)
+        for (string? command; (command = GetInput()) != "exit";)
         {
-            Console.Out.WriteLine($"{command}: command not found");
+            var arguments = ParseCommand(command);
+            if (arguments.Length == 0)
+                continue;
+            if (arguments[0] == "echo")
+                Echo(arguments);
+            else
+                Console.Out.WriteLine($"{command}: command not found");
         }
-        
     }
 
     private static string? GetInput()
     {
         Console.Write("$ ");
         return Console.ReadLine();
+    }
+
+    private static void Echo(string[] input)
+    {
+        var content = input.Skip(1).ToArray();
+        Console.WriteLine(string.Join(" ", content));
+    }
+
+    private static string[] ParseCommand(string? input)
+    {
+        return string.IsNullOrWhiteSpace(input) ? [] : input.Split(' ');
     }
 }
