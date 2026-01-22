0fb55cc6b9892b53fd2ffc77c9550ef6502a281e
Kevin <kevin.mchugh81@googlemail.com>
2026-01-22 17:18:23 +0000

    Thirteenth Task

--- src/Utils/RegExs.cs
+++ src/Utils/RegExs.cs
@@ -3,7 +3,7 @@ internal static class RegExs
 {
     internal const string QuotedPattern = @"(?<=')[^']*(?='|$)";
 
-    internal const string DoubleQuotedPattern = @"(?<="") [^""]* (?=""|$)";
+    internal const string DoubleQuotedPattern = "(?<=\")[^\"]*(?=\")";
     
     internal const string SpacePattern = @"\s+";
 }
