50276ab1cccbd224a60fdd6373e3f7cf799ba29f
Kevin <kevin.mchugh81@googlemail.com>
2026-01-26 08:55:31 +0000

    Fourteenth Task

--- src/Extensions/StringExtensions.cs
+++ src/Extensions/StringExtensions.cs
@@ -1,4 +1,6 @@
-﻿internal static class StringExtensions
+﻿using System.Text.RegularExpressions;
+
+internal static class StringExtensions
 {
     private static readonly List<char> Delimiters = ['\'', '"'];
 
@@ -6,82 +8,91 @@
     {
         var result = new List<string>();
 
-        return string.IsNullOrWhiteSpace(str) ? result : GetBetweenDelimiters(str);
+        if (string.IsNullOrWhiteSpace(str))
+            return result;
+
+        if (str.Contains(Delimiters[0]) || str.Contains(Delimiters[1]))
+        {
+            return GetBetweenDelimiters(str);
+        }
+
+        str = Regex.Replace(str, @"(\\{2})|\\", m => m.Value == @"\\" ? @"\" : "");
+        return str.Split(" ").ToList().Where(s => !string.IsNullOrWhiteSpace(s)).ToList();
     }
 
     private static List<string> GetBetweenDelimiters(string str)
     {
         var result = new List<string>();
-                var insideDelimiter = false;
-                var currentString = string.Empty;
-                var currentDelimiter = char.MinValue;
-                var escapeNextChar = false;
-        
-                for (var x = 0; x < str.Length; x++)
+        var insideDelimiter = false;
+        var currentString = string.Empty;
+        var currentDelimiter = char.MinValue;
+        var escapeNextChar = false;
+
+        for (var x = 0; x < str.Length; x++)
+        {
+            //Outside a delimiter and char is an escape char, which you aren't already escaping
+            if (!insideDelimiter && str[x] == '\\' && !escapeNextChar)
+            {
+                escapeNextChar = true;
+                continue;
+            }
+
+            //Inside a delimiter and char doesn't close it.
+            if (insideDelimiter && str[x] != currentDelimiter)
+                currentString += str[x];
+            //Not inside a delimiter and the char opens delimiter.
+            else if (!insideDelimiter && Delimiters.Contains(str[x]) && currentDelimiter != str[x] && !escapeNextChar)
+            {
+                //Store current string before, opening new one.
+                if (!string.IsNullOrWhiteSpace(currentString) && x - 1 >= 0 && !Delimiters.Contains(str[x - 1]) &&
+                    (x + 1 <= str.Length - 1 && !Delimiters.Contains(str[x + 1])))
                 {
-                    //Outside a delimiter and char is an escape char, which you aren't already escaping
-                    if (!insideDelimiter && str[x] == '\\' && !escapeNextChar)
-                    {
-                        escapeNextChar = true;
-                        continue;
-                    }
-        
-                    //Inside a delimiter and char doesn't close it.
-                    if (insideDelimiter && str[x] != currentDelimiter)
-                        currentString += str[x];
-                    //Not inside a delimiter and the char opens delimiter.
-                    else if (!insideDelimiter && Delimiters.Contains(str[x]) && currentDelimiter != str[x] && !escapeNextChar)
-                    {
-                        //Store current string before, opening new one.
-                        if (!string.IsNullOrWhiteSpace(currentString) && x - 1 >= 0 && !Delimiters.Contains(str[x - 1]) &&
-                            (x + 1 <= str.Length - 1 && !Delimiters.Contains(str[x + 1])))
-                        {
-                            result.Add(currentString);
-                            currentString = string.Empty;
-                        }
-        
-                        currentDelimiter = str[x];
-                        insideDelimiter = true;
-                        continue;
-                    }
-                    //Inside a delimiter and char closes delimiter
-                    else if (insideDelimiter && Delimiters.Contains(str[x]) && currentDelimiter == str[x])
-                    {
-                        //Store current string before closing.
-                        if (!string.IsNullOrWhiteSpace(currentString)
-                            && ((x + 1 <= str.Length - 1 && !Delimiters.Contains(str[x + 1]) && str[x] != str[x - 1]) ||
-                                x == str.Length - 1))
-                        {
-                            result.Add(currentString);
-                            currentString = string.Empty;
-                        }
-        
-                        currentDelimiter = char.MinValue;
-                        insideDelimiter = false;
-                        continue;
-                    }
-                    //If you aren't inside a delimiter and this isn't one, add none-whitespace chars
-                    else if (!insideDelimiter && (!Delimiters.Contains(str[x]) || escapeNextChar))
-                    {
-                        if (currentString.Count(y => y == '"') % 2 != 0)
-                        {
-                            currentString += str[x];
-                        }
-                        else
-                        {
-                            currentString += char.IsWhiteSpace(str[x]) && !escapeNextChar ? string.Empty : str[x];
-                        }
-                        
-                        
-                        if (escapeNextChar) escapeNextChar = false;
-                    }
-        
-        
-                    //If this is the end then store remaining string.
-                    if (!string.IsNullOrWhiteSpace(currentString) && x == str.Length - 1)
-                        result.Add(currentString);
+                    result.Add(currentString);
+                    currentString = string.Empty;
                 }
-        
-                return result;
+
+                currentDelimiter = str[x];
+                insideDelimiter = true;
+                continue;
+            }
+            //Inside a delimiter and char closes delimiter
+            else if (insideDelimiter && Delimiters.Contains(str[x]) && currentDelimiter == str[x])
+            {
+                //Store current string before closing.
+                if (!string.IsNullOrWhiteSpace(currentString)
+                    && ((x + 1 <= str.Length - 1 && !Delimiters.Contains(str[x + 1]) && str[x] != str[x - 1]) ||
+                        x == str.Length - 1))
+                {
+                    result.Add(currentString);
+                    currentString = string.Empty;
+                }
+
+                currentDelimiter = char.MinValue;
+                insideDelimiter = false;
+                continue;
+            }
+            //If you aren't inside a delimiter and this isn't one, add none-whitespace chars
+            else if (!insideDelimiter && (!Delimiters.Contains(str[x]) || escapeNextChar))
+            {
+                if (currentString.Count(y => y == '"') % 2 != 0)
+                {
+                    currentString += str[x];
+                }
+                else
+                {
+                    currentString += char.IsWhiteSpace(str[x]) && !escapeNextChar ? string.Empty : str[x];
+                }
+
+
+                if (escapeNextChar) escapeNextChar = false;
+            }
+
+
+            //If this is the end then store remaining string.
+            if (!string.IsNullOrWhiteSpace(currentString) && x == str.Length - 1)
+                result.Add(currentString);
+        }
+
+        return result;
     }
 }
